c7f139f220a5e70b8a561784d4fc671a45461fd0
Vincent Vu <172068404+rubixvi@users.noreply.github.com>
2026-03-29 15:31:47 +1100

    Refactor id type check in initialization.ts
    Signed-off-by: Vincent Vu <172068404+rubixvi@users.noreply.github.com>

--- src/lib/initialization.ts
+++ src/lib/initialization.ts
@@ -88,8 +88,8 @@ const syncExistingDocuments = async (
           .filter((doc): doc is BaseDocument => {
             if (typeof doc !== 'object' || doc === null) return false
             if (!('id' in doc)) return false
-            return (typeof (doc as Record<string, unknown>).id === 'string' ||
-		   typeof (doc as Record<string, unknown>).id === 'number')
+			const id = (doc as Record<string, unknown>).id
+			return typeof id === 'string' || typeof id === 'number'
           })
           .map((doc) => mapToTypesense(doc, collectionSlug, config))
           .filter((doc): doc is Record<string, number | string> => doc !== null)
